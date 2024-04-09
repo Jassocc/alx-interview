128c6d059b0fc03148f3eda2f99ea01290e71df9
Jassocc <clnearle@gmail.com>
2024-04-09 09:55:50 +0000

    task 0

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -15,6 +15,6 @@ def pascal_triangle(n):
         row = [1]
         for j in range(1, i):
             row.append(triangle[i - 1][j - 1] + triangle[i - 1][j])
-        row.append(1);
+        row.append(1)
         triangle.append(row)
     return triangle
