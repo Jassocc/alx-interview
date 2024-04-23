0a2489bcb133b67d8c6b1698dc8d9075a7bde708
Jassocc <clnearle@gmail.com>
2024-04-23 06:52:30 +0000

    task 0

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -8,13 +8,21 @@ def minOperations(n):
     """
     calculates the fewest number of operations needed
     """
-    if n <= 1:
+    if not isinstance(n, int):
         return 0
-    dp = [float('inf')] * (n + 1)
-    dp[1] = 0
-    for i in range(2, n + 1):
-        if n % i == 0:
-            dp[i] = min(dp[i], dp[n // i] + i)
-        for j in range(1, i // 2 + 1):
-            dp[i] = min(dp[i], dp[j] + dp[i - j] + 1)
-    return dp[n]
+    op_c = 0
+    clip = 0
+    d = 1
+    while d < n:
+        if clip == 0:
+            clip = d
+            d += clip
+            op_c += 2
+        elif n - d > 0 and (n - d) % d == 0:
+            clip = d
+            d += clip
+            op_c += 2
+        elif clip > 0:
+            d += clip
+            op_c += 1
+    return op_c
