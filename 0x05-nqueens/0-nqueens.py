2c7c0d20d284a96ceeffad04886685af89598a91
Jassocc <clnearle@gmail.com>
2024-05-15 12:11:51 +0000

    nqueen

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -5,27 +5,29 @@ script for task 0
 import sys
 
 
-def is_safe(board, row, col, n):
+def is_safe(board, row, col, N):
     """
     check if is safe
     """
     for i in range(row):
-        if (board[i] == col or board[i] - i == col - row or
-            board[i] + i == col + row):
+        if (board[i] == col or board[i] - i == col - row
+                or board[i] + i == col + row):
             return False
     return True
 
-def solve_n_queens(board, row, n):
+
+def solve_n_queens(board, row, N):
     """
     solves the n queens
     """
-    if row == n:
-        print([[i, board[i]] for i in range(n)])
+    if row == N:
+        print([[i, board[i]] for i in range(N)])
         return
-    for col in range(n):
-        if is_safe(board, row, col, n):
+    for col in range(N):
+        if is_safe(board, row, col, N):
             board[row] = col
-            solve_n_queens(board, row + 1, n)
+            solve_n_queens(board, row + 1, N)
+
 
 def main():
     """
@@ -35,14 +37,14 @@ def main():
         print("Usage: nqueens N", file=sys.stderr)
         sys.exit(1)
     try:
-        n = int(sys.argv[1])
+        N = int(sys.argv[1])
     except ValueError:
         print("N must be a number", file=sys.stderr)
         sys.exit(1)
-    if n < 4:
+    if N < 4:
         print("N must be at least 4", file=sys.stderr)
         sys.exit(1)
-    solve_n_queens([-1] * n, 0, n)
+    solve_n_queens([-1] * N, 0, N)
 
 
 if __name__ == "__main__":
