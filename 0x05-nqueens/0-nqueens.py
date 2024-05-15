7a197373bea6b02c3251d7fd2754b55bd4b0b0e5
Jassocc <clnearle@gmail.com>
2024-05-15 12:24:17 +0000

    task 0

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -16,36 +16,40 @@ def is_safe(board, row, col, N):
     return True
 
 
-def solve_n_queens(board, row, N):
+def solve_n_queens(N):
     """
     solves the n queens
     """
-    if row == N:
-        print([[i, board[i]] for i in range(N)])
-        return
-    for col in range(N):
-        if is_safe(board, row, col, N):
-            board[row] = col
-            solve_n_queens(board, row + 1, N)
-
-
-def main():
-    """
-    main function
-    """
-    if len(sys.argv) != 2:
-        print("Usage: nqueens N", file=sys.stderr)
-        sys.exit(1)
-    try:
-        N = int(sys.argv[1])
-    except ValueError:
-        print("N must be a number", file=sys.stderr)
-        sys.exit(1)
     if N < 4:
         print("N must be at least 4", file=sys.stderr)
         sys.exit(1)
-    solve_n_queens([-1] * N, 0, N)
+    if not isinstance(N, int):
+        print("N must be a number", file=sys.stderr)
+        sys.exit(1)
+    if len(sys.argv) != 2:
+        print("Usage: nqueens N", file=sys.stderr)
+        sys.exit(1)
+
+    def solve(row):
+        """
+        solves work
+        """
+        if row == N:
+            print(board)
+            return
+        for col in range(N):
+            if is_safe(board, row, col):
+                board[row] = col
+                solve(row + 1)
+    board = [-1] * N
+    solve(0)
 
 
 if __name__ == "__main__":
-    main()
+    if len(sys.argv) == 2:
+        try:
+            N = int(sys.argv[1])
+            solve_n_queens(N)
+        except ValueError:
+            print("N must be a number", file=sys.stderr)
+            sys.exit(1)
